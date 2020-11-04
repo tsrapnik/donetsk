91d7efda57e3e05d258a3e52bbe5aab591e280c9
tsrapnik <yannick-peeters@outlook.com>
2020-11-04 20:36:16 +0100

    improved drawing algorithm.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use petgraph::graph::{DiGraph, NodeIndex};
 
-use nalgebra::Vector2;
-
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 
@@ -17,17 +15,17 @@ fn main() {
     let mut folder_tree = DiGraph::new();
     let root = folder_tree.add_node(folder_tree::Node {
         name: "C:/Users/tsrapnik".to_string(),
-        position: Vector2::new(0.0, 0.0),
-        size: Vector2::from([folder_tree::FOLDER_SIDE; 2]),
+        position: [-0.9, -0.9],
+        size: [folder_tree::FOLDER_SIDE; 2],
         color: [0.5, 0.2, 0.2],
     });
 
     folder_tree::browse_folder(&mut folder_tree, root);
-    // folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(1));
-    // folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(2));
-    // folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(3));
-    // folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(4));
-    // folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(5));
+    folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(1));
+    folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(2));
+    folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(3));
+    folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(4));
+    folder_tree::browse_folder(&mut folder_tree, NodeIndex::new(5));
 
     let mut window_resized = false;
     event_loop.run(move |event, _, control_flow| match event {
@@ -50,14 +48,9 @@ fn main() {
             let mut character_buffer = Vec::new();
             folder_tree::draw(
                 &folder_tree,
-                root,
                 &mut character_buffer,
                 &mut rectangle_buffer,
             );
-
-            for rectangle in &rectangle_buffer {
-                println!("{:?}", rectangle);
-            }
 
             println!("time lost by cpu scheduling is ignored.");
             println!("cpu processing time: {:?}", frame_start.elapsed());
--- src/modules/folder_tree.rs
+++ src/modules/folder_tree.rs
@@ -1,7 +1,6 @@
-use nalgebra::Vector2;
 use petgraph::{
     graph::{DiGraph, NodeIndex},
-    visit::Bfs,
+    Direction,
 };
 use std::fs;
 
@@ -13,8 +12,8 @@ pub const FOLDER_MARGIN: f32 = 0.01;
 #[derive(Debug)]
 pub struct Node {
     pub name: String,
-    pub position: Vector2<f32>,
-    pub size: Vector2<f32>,
+    pub position: [f32; 2],
+    pub size: [f32; 2],
     pub color: [f32; 3],
 }
 
@@ -29,22 +28,44 @@ pub fn browse_folder(folder_tree: &mut DiGraph<Node, ()>, parent_node: NodeIndex
 
         //calculate how large the parent folder should be to fit subfolders.
         let subfolder_row_count = (subfolder_count as f64).sqrt().ceil() as f32;
-        let parent_size = {
-            let parent_side = subfolder_row_count * (FOLDER_SIDE + FOLDER_MARGIN) + FOLDER_MARGIN;
-            Vector2::from([parent_side; 2])
-        };
+        let new_parent_side_length = subfolder_row_count * (FOLDER_SIDE + FOLDER_MARGIN) + FOLDER_MARGIN;
+        let offset = [
+            new_parent_side_length - folder_tree[parent_node].size[0],
+            new_parent_side_length - folder_tree[parent_node].size[1],
+        ];
 
-        //move existing folders to make place for parent folder.
-        let parent_far_corner = folder_tree[parent_node].position + folder_tree[parent_node].size;
-        for node in folder_tree.node_indices() {
-            //only folders in the quadrant with x and y values greater than the parent x and y should be moved.
-            if folder_tree[node].position > parent_far_corner {
-                folder_tree[node].position += parent_size;
+        //move the siblings of the parent node out of the way and do the same for its uncles and
+        //aunts, grand uncles and grand aunts, etc.
+        let mut current_node = parent_node;
+        loop {
+            //we only need to move the siblings past bottom right corner of current node away.
+            let current_node_far_corner = [
+                folder_tree[current_node].position[0] + folder_tree[current_node].size[0],
+                folder_tree[current_node].position[1] + folder_tree[current_node].size[1],
+            ];
+
+            //expand the node itself.
+            folder_tree[current_node].size[0] += offset[0];
+            folder_tree[current_node].size[1] += offset[1];
+
+            if let Some(current_node_parent) = folder_tree.neighbors_directed(current_node, Direction::Incoming).next() {
+                //move siblings out of the way.
+                let mut sibling_edges = folder_tree.neighbors_directed(current_node_parent, Direction::Outgoing).detach();
+                while let Some(sibling_edge) = sibling_edges.next_edge(&folder_tree) {
+                    let (_, sibling) = folder_tree.edge_endpoints(sibling_edge).unwrap();
+                    if folder_tree[sibling].position[0] > current_node_far_corner[0] {
+                        folder_tree[sibling].position[0] += offset[0];
+                    }
+                    if folder_tree[sibling].position[1] > current_node_far_corner[1] {
+                        folder_tree[sibling].position[1] += offset[1];
+                    }
+                }
+                current_node = current_node_parent;
+            } else
+            {
+                break;
             }
         }
-
-        //enlarge the parent folder to fit the subfolders.
-        folder_tree[parent_node].size = parent_size;
 
         //decide color of the subfolders.
         use rand::Rng;
@@ -57,17 +78,22 @@ pub fn browse_folder(folder_tree: &mut DiGraph<Node, ()>, parent_node: NodeIndex
 
         //place subfolders in parent folder.
         for (index, subfolder) in subfolders.into_iter().enumerate() {
-            let offset = Vector2::new(
+            let offset = [
                 (index as f32) % subfolder_row_count,
                 ((index as f32) / subfolder_row_count).floor(),
-            );
-            let subfolder_position = folder_tree[parent_node].position
-                + Vector2::from([FOLDER_MARGIN; 2])
-                + offset.component_mul(&Vector2::from([FOLDER_SIDE + FOLDER_MARGIN; 2]));
+            ];
+            let subfolder_position = [
+                folder_tree[parent_node].position[0]
+                    + FOLDER_MARGIN
+                    + offset[0] * (FOLDER_SIDE + FOLDER_MARGIN),
+                folder_tree[parent_node].position[1]
+                    + FOLDER_MARGIN
+                    + offset[1] * (FOLDER_SIDE + FOLDER_MARGIN),
+            ];
             let new_node = folder_tree.add_node(Node {
                 name: subfolder.path().into_os_string().into_string().unwrap(),
                 position: subfolder_position,
-                size: Vector2::from([FOLDER_SIDE; 2]),
+                size: [FOLDER_SIDE; 2],
                 color: color,
             });
             folder_tree.add_edge(parent_node, new_node, ());
@@ -77,16 +103,17 @@ pub fn browse_folder(folder_tree: &mut DiGraph<Node, ()>, parent_node: NodeIndex
 
 pub fn draw(
     folder_tree: &DiGraph<Node, ()>,
-    root: NodeIndex,
     text_character_buffer: &mut Vec<graphics::TextCharacter>,
     rectangle_buffer: &mut Vec<graphics::Rectangle>,
 ) {
-    let mut bfs = Bfs::new(&*folder_tree, root);
-    while let Some(node) = bfs.next(&*folder_tree) {
+    //at this stage rectangles are already ordered from back to front, so we can just loop through
+    //them.
+    for node in folder_tree.node_indices() {
         rectangle_buffer.push(graphics::Rectangle {
             position: folder_tree[node].position.into(),
             size: folder_tree[node].size.into(),
             color: folder_tree[node].color,
+
             padding: 0.0,
         });
         graphics::push_string(
