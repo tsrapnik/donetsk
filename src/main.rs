6598820425d9d26c4e7ae696c2c09745be243ccf
tsrapnik <yannick-peeters@outlook.com>
2021-11-20 16:47:10 +0100

    Use linux path.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
     let mut folder_tree = DiGraph::new();
     let root = folder_tree.add_node(folder_tree::Node {
-        name: "C:/Users/tsrapnik".to_string(),
+        name: "/home/tsrapnik".to_string(),
         position: [-0.9, -0.9],
         size: [folder_tree::FOLDER_SIDE; 2],
         color: [0.5, 0.2, 0.2],
